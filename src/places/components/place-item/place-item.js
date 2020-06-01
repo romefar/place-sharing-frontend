53e7c409c483e59d9fef7aa187c28c5417d24172
romefar <vlad.romanovr@gmail.com>
2020-06-01 19:46:37 +0300

    Added del modal

--- src/places/components/place-item/place-item.js
+++ src/places/components/place-item/place-item.js
@@ -1,19 +1,28 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment, useContext } from 'react'
 import PropTypes from 'prop-types'
 
 import Button from '../../../shared/form-elements/button'
 import Card from '../../../shared/UI-elements/card'
 import Modal from '../../../shared/UI-elements/modal'
 import Map from '../../../shared/UI-elements/map'
+import AuthContext from '../../../shared/context/auth-context'
 
 import './place-item.css'
 
 const PlaceItem = ({ id, imageUrl, title, description, address, creatorId, location }) => {
+  const { isLoggedIn } = useContext(AuthContext)
   const [showMap, setMapVisibility] = useState(false)
+  const [showConfirmModal, setConfirmModalVisibility] = useState(false)
 
   const showMapHandler = () => setMapVisibility(true)
-
   const closeMapHandler = () => setMapVisibility(false)
+
+  const showDeleteModalHandler = () => setConfirmModalVisibility(true)
+  const closeDeleteModalHandler = () => setConfirmModalVisibility(false)
+  const confirmDeleteHandler = () => {
+    closeDeleteModalHandler()
+    console.log('Deleting')
+  }
 
   return (
     <Fragment>
@@ -28,6 +37,19 @@ const PlaceItem = ({ id, imageUrl, title, description, address, creatorId, locat
           <Map center={location} zoom={12}/>
         </div>
       </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={closeDeleteModalHandler}
+        headerTitle="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footerContent={
+          <Fragment>
+            <Button inverse onClick={closeDeleteModalHandler}>Cancel</Button>
+            <Button danger onClick={confirmDeleteHandler}>Delete</Button>
+          </Fragment>
+        }>
+        <p>Do you want to delete this place? Please note that it cannot be undone. </p>
+      </Modal>
       <li className="place-item-actions">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -40,8 +62,8 @@ const PlaceItem = ({ id, imageUrl, title, description, address, creatorId, locat
           </div>
           <div className="place-item__actions">
             <Button inverse onClick={showMapHandler}>View on map</Button>
-            <Button to={`/places/${id}`}>Edit</Button>
-            <Button danger>Delete</Button>
+            { isLoggedIn && <Button to={`/places/${id}`}>Edit</Button>}
+            { isLoggedIn && <Button danger onClick={showDeleteModalHandler}>Delete</Button>}
           </div>
         </Card>
       </li>
