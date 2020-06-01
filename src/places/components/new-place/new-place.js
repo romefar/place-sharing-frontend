0bfb8de60b2900b1157c31f824e24f16c8d8f50a
romefar <vlad.romanovr@gmail.com>
2020-06-01 19:46:05 +0300

    Added auto-gen form

--- src/places/components/new-place/new-place.js
+++ src/places/components/new-place/new-place.js
@@ -1,13 +1,61 @@
 import React from 'react'
 
 import Input from '../../../shared/form-elements/input'
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../../shared/utils/validators'
+import Button from '../../../shared/form-elements/button'
+import useForm from '../../../shared/hooks/form-hook'
 
 import './new-place.css'
 
 const NewPlace = () => {
+  const [formState, inputHandler] = useForm({
+    title: {
+      value: '',
+      isValid: false
+    },
+    description: {
+      value: '',
+      isValid: false
+    },
+    address: {
+      value: '',
+      isValid: false
+    }
+  })
+
+  const placeSubmitHandler = e => {
+    e.preventDefault()
+  }
+
   return (
-    <form className="place-form">
-      <Input element='input' type='text' label='Title' />
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <Input
+        id='title'
+        element='input'
+        type='text'
+        label='Title'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please provide a valid title."
+        onInput={inputHandler}
+      />
+      <Input
+        id='description'
+        element='textarea'
+        label='Description'
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id='address'
+        element='input'
+        label='Address'
+        type='text'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>Add place</Button>
     </form>
   )
 }
