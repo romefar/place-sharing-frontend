b134c902697dc1aacbb1d5ff6899758440366137
romefar <vlad.romanovr@gmail.com>
2020-06-01 19:50:00 +0300

    Added prite routes

--- src/shared/navigation/nav-links/nav-links.js
+++ src/shared/navigation/nav-links/nav-links.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+
+import AuthContext from '../../context/auth-context'
 
 import './nav-links.css'
 
 const NavLinks = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext)
   return (
     <ul className="nav-links">
       <li>
         <NavLink to="/" exact>All users</NavLink>
       </li>
-      <li>
-        <NavLink to="/ul2/places">My places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">Add place</NavLink>
-      </li>
-      <li>
-        <NavLink to="/signin">Auth</NavLink>
-      </li>
+      {
+        isLoggedIn && <li>
+          <NavLink to="/ul2/places">My places</NavLink>
+        </li>
+      }
+      {
+        isLoggedIn && <li>
+          <NavLink to="/places/new">Add place</NavLink>
+        </li>
+      }
+      {
+        !isLoggedIn && <li>
+          <NavLink to="/auth">Auth</NavLink>
+        </li>
+      }
+      {
+        isLoggedIn && <li>
+          <button onClick={logout} to="/auth">Logout</button>
+        </li>
+      }
     </ul>
   )
 }
