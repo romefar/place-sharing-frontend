e56043bafa31113b9a5e130616ae013015dd45d0
romefar <vlad.romanovr@gmail.com>
2020-06-03 21:40:12 +0300

    Fixed private routes

--- src/app/app.js
+++ src/app/app.js
@@ -13,9 +13,16 @@ import './app.css'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userId, setUserId] = useState(false)
 
-  const login = useCallback(() => setIsLoggedIn(true), [])
-  const logout = useCallback(() => setIsLoggedIn(false), [])
+  const login = useCallback((userId) => {
+    setIsLoggedIn(true)
+    setUserId(userId)
+  }, [])
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+    setUserId(null)
+  }, [])
 
   let routes = null
   if (isLoggedIn) {
@@ -39,7 +46,7 @@ const App = () => {
     )
   }
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, userId }}>
       <Router>
         <MainNavigation />
         <main>
