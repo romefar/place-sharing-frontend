6cd79174ebe0bca0df288d97d3940f2c230f1368
romefar <vlad.romanovr@gmail.com>
2020-06-03 21:42:52 +0300

    Fixed place update

--- src/places/components/update-place/update-place.js
+++ src/places/components/update-place/update-place.js
@@ -1,45 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState, useContext, Fragment } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 
 import MessageBox from '../../../shared/message-box'
 import Button from '../../../shared/form-elements/button'
 import Input from '../../../shared/form-elements/input'
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../../shared/utils/validators'
 import useForm from '../../../shared/hooks/form-hook'
+import useHttpClient from '../../../shared/hooks/http-hook'
+import ErrorModal from '../../../shared/UI-elements/error-modal'
+import LoadingSpinner from '../../../shared/UI-elements/loading-spinner'
+import AuthContext from '../../../shared/context/auth-context'
 
 import './update-place.css'
 
-const PLACES = [
-  {
-    id: 'u1',
-    title: 'Emprie State Building',
-    description: 'The most famouse place in the world',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/280px-Empire_State_Building_from_the_Top_of_the_Rock.jpg',
-    address: '20 W 34th St, New York, NY 10001, USA',
-    location: {
-      lat: 40.7484445,
-      lng: -73.9878531
-    },
-    creatorId: 'ul1'
-  },
-  {
-    id: 'u2',
-    title: 'Emprie State Building 222',
-    description: 'The most famouse place in the world',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/280px-Empire_State_Building_from_the_Top_of_the_Rock.jpg',
-    address: '20 W 34th St, New York, NY 10001, USA',
-    location: {
-      lat: 40.7484445,
-      lng: -73.9878531
-    },
-    creatorId: 'ul2'
-  }
-]
-
 const UpdatePlace = () => {
   const placeId = useParams().placeId
-  const [isLoading, setLoading] = useState(true)
-
+  const { isLoading, error, sendRequest, clearError } = useHttpClient()
+  const [loadedPlace, setLoadedPlace] = useState()
+  const history = useHistory()
+  const { userId } = useContext(AuthContext)
   const [formState, inputHandler, setFormData] = useForm({
     title: {
       value: '',
@@ -51,67 +30,88 @@ const UpdatePlace = () => {
     }
   }, false)
 
-  const placeUpdateSubmitHandler = e => {
+  const placeUpdateSubmitHandler = async e => {
     e.preventDefault()
+    try {
+      await sendRequest(
+        `http://localhost:3001/api/places/${placeId}`,
+        'PATCH',
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value
+        }),
+        {
+          'Content-Type': 'application/json'
+        })
+      history.push(`/${userId}/places`)
+    } catch (error) {
+
+    }
   }
-  const place = PLACES.find(place => place.id === placeId)
 
   useEffect(() => {
-    if (place) {
-      setFormData({
-        title: {
-          value: place.title,
-          isValid: true
-        },
-        description: {
-          value: place.description,
-          isValid: true
-        }
-      }, true)
+    const fetchUserPlaces = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:3001/api/places/${placeId}`)
+        setLoadedPlace(responseData.place)
+        setFormData({
+          title: {
+            value: responseData.place.title,
+            isValid: true
+          },
+          description: {
+            value: responseData.place.description,
+            isValid: true
+          }
+        }, true)
+      } catch (error) {
+      }
     }
-
-    setLoading(false)
-  }, [setFormData, place])
-
-  if (!place) {
-    return <MessageBox text="Couldn't find the place." />
-  }
+    fetchUserPlaces()
+  }, [sendRequest, placeId, setFormData])
 
   if (isLoading) {
     return (
-      <p>Loading...</p>
+      <div className="center">
+        <LoadingSpinner />
+      </div>
     )
   }
 
-  const { title, description } = formState.inputs
+  if (!loadedPlace && !error) {
+    return <MessageBox text="Couldn't find the place." />
+  }
 
   return (
-    <form
-      className="place-form"
-      onSubmit={placeUpdateSubmitHandler}>
-      <Input
-        id='title'
-        element='input'
-        type='text'
-        label='Title'
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please provide a valid title."
-        onInput={inputHandler}
-        initialValue={title.value}
-        initialValid={title.isValid}
-      />
-      <Input
-        id='description'
-        element='textarea'
-        label='Description'
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-        initialValue={description.value}
-        initialValid={description.isValid}
-      />
-      <Button type="submit" disabled={!formState.isValid}>Update place</Button>
-    </form>
+    <Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {!isLoading && loadedPlace && <form
+        className="place-form"
+        onSubmit={placeUpdateSubmitHandler}>
+        <Input
+          id='title'
+          element='input'
+          type='text'
+          label='Title'
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please provide a valid title."
+          onInput={inputHandler}
+          initialValue={loadedPlace.title}
+          initialValid={true}
+        />
+        <Input
+          id='description'
+          element='textarea'
+          label='Description'
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+          initialValue={loadedPlace.description}
+          initialValid={true}
+        />
+        <Button type="submit" disabled={!formState.isValid}>Update place</Button>
+      </form>}
+    </Fragment>
   )
 }
 
