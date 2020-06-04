6e7795a3711ce1e2a260f40a22730b9a89a0e7fc
romefar <vlad.romanovr@gmail.com>
2020-06-04 20:15:42 +0300

    Fixed user card image

--- src/user/components/user-item/user-item.js
+++ src/user/components/user-item/user-item.js
@@ -12,7 +12,7 @@ const UserItem = ({ id, image, name, placeCount }) => {
       <Card className="user-item__content">
         <Link to={`/${id}/places`}>
           <div className="user-item__image">
-            <Avatar image={image} alt={name}/>
+            <Avatar image={`http://localhost:3001/${image}`} alt={name}/>
           </div>
           <div className="user-item__info">
             <h2>{name}</h2>
