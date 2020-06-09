987b36fbdad1aa21c148615d9c5dbeff4498fca4
romefar <vlad.romanovr@gmail.com>
2020-06-09 21:00:10 +0300

    Added markers support

--- src/shared/UI-elements/map/map.js
+++ src/shared/UI-elements/map/map.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
+import httpConfig from '../../../config/http-config'
 
 import './map.css'
 
@@ -7,19 +8,36 @@ const Map = ({ className = '', center, zoom }) => {
   const mapRef = useRef()
 
   useEffect(() => {
-    // eslint-disable-next-line no-unused-vars
-    const map = new window.ol.Map({
+    const ol = window.ol
+    const map = new ol.Map({
       target: mapRef.current.id,
       layers: [
-        new window.ol.layer.Tile({
-          source: new window.ol.source.OSM()
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
         })
       ],
-      view: new window.ol.View({
-        center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+      view: new ol.View({
+        center: ol.proj.fromLonLat([center.lng, center.lat]),
         zoom: zoom
       })
     })
+
+    const layer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [new ol.Feature({
+          geometry: new ol.geom.Point(ol.proj.transform([parseFloat(center.lng), parseFloat(center.lat)], 'EPSG:4326', 'EPSG:3857'))
+        })]
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 0.5],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+          src: `${httpConfig.getDomain()}/assets/marker.png`
+        })
+      })
+    })
+    map.addLayer(layer)
   }, [center, zoom])
 
   return (
