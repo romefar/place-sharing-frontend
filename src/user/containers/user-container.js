ff9470bd23ef998e29e945c4fea008aaece421f8
romefar <vlad.romanovr@gmail.com>
2020-06-09 21:01:12 +0300

    Fixed fetch url

--- src/user/containers/user-container.js
+++ src/user/containers/user-container.js
@@ -4,6 +4,7 @@ import UsersList from '../components/users-list'
 import ErrorModal from '../../shared/UI-elements/error-modal'
 import LoadingSpinner from '../../shared/UI-elements/loading-spinner'
 import useHttpClient from '../../shared/hooks/http-hook'
+import httpConfig from '../../config/http-config'
 
 const UserContainer = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient()
@@ -11,7 +12,7 @@ const UserContainer = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const responseData = await sendRequest('http://localhost:3001/api/users')
+        const responseData = await sendRequest(`${httpConfig.getDomain()}/api/users`)
         setLoadedUsers(responseData.users)
       } catch (error) {
       }
