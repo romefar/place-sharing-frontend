279c8af8711333b3422b668c99b0c496d18c51fa
romefar <vlad.romanovr@gmail.com>
2020-06-09 21:01:02 +0300

    Fixed fetch url

--- src/user/components/user-item/user-item.js
+++ src/user/components/user-item/user-item.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 
 import Avatar from '../../../shared/UI-elements/avatar'
 import Card from '../../../shared/UI-elements/card'
+import httpConfig from '../../../config/http-config'
+
 import './user-item.css'
 
 const UserItem = ({ id, image, name, placeCount }) => {
@@ -12,7 +14,7 @@ const UserItem = ({ id, image, name, placeCount }) => {
       <Card className="user-item__content">
         <Link to={`/${id}/places`}>
           <div className="user-item__image">
-            <Avatar image={`http://localhost:3001/${image}`} alt={name}/>
+            <Avatar image={`${httpConfig.getDomain()}/${image}`} alt={name}/>
           </div>
           <div className="user-item__info">
             <h2>{name}</h2>
