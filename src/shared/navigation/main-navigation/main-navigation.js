1c444f66fdd2625d077580744eed034b252e8ab0
romefar <vlad.romanovr@gmail.com>
2020-06-09 20:59:16 +0300

    Changed header title

--- src/shared/navigation/main-navigation/main-navigation.js
+++ src/shared/navigation/main-navigation/main-navigation.js
@@ -37,7 +37,7 @@ const MainNavigation = () => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">Your places</Link>
+          <Link to="/">Our places</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
